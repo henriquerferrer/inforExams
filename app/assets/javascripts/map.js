c72ae17d76cf46b3beb5707840486346f94d2fd0
Henrique Ferrer <hvrbferrer@gmail.com>
2018-06-01 17:55:49 +0100

    puts new way of recieving current location

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,20 +1,16 @@
 $(window).load(function() {
   handler = Gmaps.build('Google');
-  x = navigator.geolocation;
-  x.getCurrentPosition(success);
-  function success(position){
-    handler.buildMap({ provider: {zoom: 18}, internal: {id: 'map'}}, function(){
-      markers = handler.addMarkers([
-        {
-          "lat": position.coords.latitude,
-          "lng": position.coords.longitude,
-          "infowindow": "hello!"
-        }
-      ]);
-      handler.bounds.extendWith(markers);
-      handler.map.centerOn({ lat: position.coords.latitude, lng: position.coords.longitude })
-    });
-  }
+  handler.buildMap({ provider: {zoom: 18}, internal: {id: 'map'}}, function(){
+    markers = handler.addMarkers([
+      {
+        "lat": gon.lat,
+        "lng": gon.lng,
+        "infowindow": "hello!"
+      }
+    ]);
+    handler.bounds.extendWith(markers);
+    handler.map.centerOn({ lat: gon.lat, lng: gon.lng })
+  });
 });
 
 $(document).ready(function(){
