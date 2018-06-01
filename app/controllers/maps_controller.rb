2c4d918efb2facfafd308e70e3ebc7e2093c8114
Henrique Ferrer <hvrbferrer@gmail.com>
2018-06-01 06:12:13 +0100

    get app ready for deploy

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -2,10 +2,10 @@ class MapsController < ApplicationController
 
   def randomize
     @client = GooglePlaces::Client.new("AIzaSyCVcuPwW5vDnPwhBMVvf322lmGcZwqtCeY")
-    # lat = request.location.latitude
-    # lng = request.location.longitude 
-    # spots = @client.spots(lat, lng, radius: 1000, types: 'restaurant') 
-    spots = @client.spots(40.202738, -8.401090, radius: 1000, types: 'restaurant') #remove this line for deployment
+    lat = request.location.latitude
+    lng = request.location.longitude 
+    spots = @client.spots(lat, lng, radius: 1000, types: 'restaurant') 
+    #spots = @client.spots(40.202738, -8.401090, radius: 1000, types: 'restaurant') #remove this line for deployment
     @randomSpot = spots[rand(spots.length)]
     gon.randomSpot = @randomSpot
     if @randomSpot
