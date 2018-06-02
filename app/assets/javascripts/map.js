62598b71ec2cbc06332f34547787310729cdeb01
henriquerferrer <hvrbferrer@gmail.com>
2018-06-02 19:38:45 +0100

    go back in code cause discovered that map only works in secure origins like https

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -6,14 +6,16 @@ $(window).load(function() {
     handler.buildMap({ provider: {zoom: 18}, internal: {id: 'map'}}, function(){
       markers = handler.addMarkers([
         {
-          "lat": gon.lat,
-          "lng": gon.lng,
+          "lat": position.coords.latitude,
+          "lng": position.coords.longitude,
           "infowindow": "hello!"
         }
       ]);
       handler.bounds.extendWith(markers);
-      handler.map.centerOn({ lat: gon.lat, lng: gon.lng })
+      handler.map.centerOn({ lat: position.coords.latitude, lng: position.coords.longitude })
     });
+    $("#place-lat").html(position.coords.latitude)
+    $("#place-lng").html(position.coords.longitude)
   }
 });
 
--- app/controllers/initials_controller.rb
+++ app/controllers/initials_controller.rb
@@ -1,6 +1,4 @@
 class InitialsController < ApplicationController
   def home
-    gon.lat = request.location.latitude
-    gon.lng = request.location.longitude
   end
 end
