dd3d95b31659f4f53582cdd2d6dbdfb47fd4a820
henriquerferrer <hvrbferrer@gmail.com>
2018-06-02 19:21:58 +0100

    try to back trace the error

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(window).load(function() {
   handler = Gmaps.build('Google');
   x = navigator.geolocation;
   x.getCurrentPosition(success);
@@ -18,33 +18,3 @@ $(document).ready(function() {
     $("#place-lng").html(position.coords.longitude)
   }
 });
-
-$(document).ready(function(){
-  $("#result").on('show', function(){
-    console.log(gon.randomSpot)
-    handler = Gmaps.build('Google');
-    handler.buildMap({ provider: {zoom: 18}, internal: {id: 'map'}}, function(){
-      markers = handler.addMarkers([
-        {
-          "lat": gon.randomSpot.lat,
-          "lng": gon.randomSpot.lng,
-          "infowindow": "hello!"
-        }
-      ]);
-      handler.bounds.extendWith(markers);
-      handler.map.centerOn({ lat: gon.randomSpot.lat, lng: gon.randomSpot.lng} );
-    });
-  });
-});
-
-function randomize(){
-  x = navigator.geolocation;
-  x.getCurrentPosition(success);
-  function success(position){
-    var latitude = position.coords.latitude
-    var longitude = position.coords.longitude
-    $.ajax({
-        url: "randomize/?latitude="+latitude+"&longitude="+longitude,
-    });
-  }
-}
